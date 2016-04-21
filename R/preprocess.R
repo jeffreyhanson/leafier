757870c5057b1b53951668e381e5a95782d27606
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2016-04-21 11:41:59 +1000

    + initial commit + probably lots of wrong stuff

---
+++ R/preprocess.R
@@ -0,0 +1,17 @@
+#' @include leafier-internal.R misc.R 
+
+#' Preprocesses Lines
+#'
+#' This function preprocesses lines
+#'
+#' @param x \code{SpatialLinesDataFrame} object
+#' @example
+#' 
+#' @export
+preprocessLines <- function(x) {
+	
+	
+	
+	return(NULL)
+}
+ 
